acbb9abbfeac4fc838e6f5b91d08220643a03926
JOELTVA2 <amendick_@hotmail.com>
2016-09-27 11:34:25 +0200

    olika

---
+++ Labbarna/Labbarna/DAL/DBUTILs.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Labbarna.DAL
+{
+    class DBUTILs
+    {
+        public static SqlConnection Connect()
+        {
+            string connsting = "Data Source = DESKTOP-THBJUU3\\SQLEXPRESS;Initial Catalog = Patient; User ID = sa; Password=hej123 ";
+            SqlConnection conn = new SqlConnection(connsting);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return conn;
+        }
+
+        public static DataSet spCarBrand(int maxPrice)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+           
+
+            cmd.CommandText = "spCarBrand";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@MaxPrice", maxPrice);
+
+            cmd.Connection = Connect();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            
+
+            da.Fill(ds);
+            return ds;
+            
+
+        }
+
+        public static DataSet spSearchVariabel(string tableName)
+        {
+            try
+            {
+
+            
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+
+
+            cmd.CommandText = "spSearchVariabel";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@TableName", tableName);
+
+            cmd.Connection = Connect();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+
+            da.Fill(ds);
+            return ds;
+        } catch (SqlException ex)
+            {
+                if (ex.Number.Equals(208))
+                {
+                    Console.WriteLine("Bad name");
+                }
+                return null;
+            }
+
+    }
+
+
+
+
+
+    }
+}
---
+++ Labbarna/Labbarna/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Labbarna.DAL;
+
+namespace Labbarna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+            DataSet ds = new DataSet();
+            //ds = DAL.DBUTILs.spCarBrand(70000);
+            ds = DAL.DBUTILs.spSearchVariabel("dept");
+
+            dataGridViewTest.DataSource = ds.Tables[0];
+
+
+          
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
